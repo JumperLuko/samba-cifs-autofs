b06b697877bb062538378c7edd43ced25ed4996e
JumperLuko <jumper.luko@gmail.com>
2022-01-26 17:41:32 -0300

    ~ English ~ Fix default server + Acept workgroup (uncommented)

--- samba-cifs-autofs.sh
+++ samba-cifs-autofs.sh
@@ -1,62 +1,65 @@
 #!/bin/bash
+# Script by JumperLuko jumperluko.github.io
+# https://wiki.archlinux.org/title/Autofs
 
-folder=Servidor
-#domain=workgroup
-
-echo -e "\n=== Mapeando servidor Samba ===\n"
-read -p "Qual IP? " ip
-read -p "Qual o usuário? " user
-read -s -p "Qual a senha? " pass
+echo -e "\n=== Mapping Samba server ===\n"
+read -p "IP: " ip
+read -p "User: " user
+read -s -p "Password: " pass
 echo ""
-read -p "Qual o nome da pasta? [padrão=Servidor] " folder
-#read -p "Qual o dominio / grupo de trabalho? [padrao=workgroup]" domain
+read -p "Folder [default=Servidor] " folder
+read -p "workgroup: (empty for without) [common=workgroup]" domain
 
-# Define diretório se existir
+if [ "$folder" == "" ];then
+    folder=Servidor
+fi
+
+# Set directory if it exists
 if [ -e "/etc/autofs/" ]; then
     autofsDir="/etc/autofs"
 elif [ -e "/etc/auto.master" ]; then
     autofsDir="/etc"
 fi
 
-# Somente escreve se não tiver o texto do caminho, $1 é texto, $2 é caminho 
-escreveTextoSeNaoExistir(){
+# Only write if there is no path text, $1 is text, $2 is path
+writeTextIfNotExist(){
     
     if ! [ -e "$2" ];then
         sudo bash -c "sudo echo $1 >> $2"
     else
-        verificaTexto=`grep "$1" "$2"`
-        if [ "$verificaTexto" == "" ];then
+        checkText=`grep "$1" "$2"`
+        if [ "$checkText" == "" ];then
             sudo bash -c "sudo echo $1 >> $2"
         fi
     fi
 }
 
-# Escreve em auto.master caminho da montagem
-escreveTextoSeNaoExistir "/mnt/samba/ $autofsDir/auto.samba --timeout 60 --browse" "$autofsDir/auto.master"
+# Write to auto.master mount path
+writeTextIfNotExist "/mnt/samba/ $autofsDir/auto.samba --timeout 60 --browse" "$autofsDir/auto.master"
 
-# Cria pasta de credenciais se não existir, e permite somente root
+# Create credential folder if it doesn't exist, and only allow root
 if ! [ -e "$autofsDir/credentials/" ]; then
     sudo mkdir $autofsDir/credentials/
     sudo chown root:root $autofsDir/credentials/
     sudo chmod 600 $autofsDir/credentials/
 fi
 
-# Monta samba
-escreveTextoSeNaoExistir "$ip-$folder -fstype=cifs,credentials=$autofsDir/credentials/$ip-$folder.txt,noperm,nounix,file_mode=0777,dir_mode=0777 ://$ip/$folder" "$autofsDir/auto.samba"
+# Mount samba
+writeTextIfNotExist "$ip-$folder -fstype=cifs,credentials=$autofsDir/credentials/$ip-$folder.txt,noperm,nounix,file_mode=0777,dir_mode=0777 ://$ip/$folder" "$autofsDir/auto.samba"
 
-# Salva as credenciais
+# Save the credentials
 if [ "$domain" == "" ];then
     sudo bash -c "echo -e \"user=$user\npassword=$pass\" > $autofsDir/credentials/$ip-$folder.txt"
 else
     sudo bash -c "sudo echo -e \"user=$user\npassword=$pass\ndomain=$domain\" > $autofsDir/credentials/$ip-$folder.txt"
 fi
 
-# Tirar permissão para usuáios não root lerem arquivo de credenciais
+# Remove permission for non-root users to read credential file
 sudo chown root:root $autofsDir/credentials/$ip-$folder.txt
 sudo chmod 600 $autofsDir/credentials/$ip-$folder.txt
 
 sudo systemctl restart autofs.service 
 sudo systemctl enable autofs.service
 
-# Limpeza de variaveis
-unset ip user pass folder domain autofsDir verificaTexto
+# Cleaning variables
+unset ip user pass folder domain autofsDir checkText
