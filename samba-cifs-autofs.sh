6fa64ef4ef5b2948e79a2745bf4a9c380e23c3a0
JumperLuko <jumper.luko@gmail.com>
2022-01-26 09:49:12 -0300

    + Funciona para distros com diretório diferente + limpeza de variaveis

--- samba-cifs-autofs.sh
+++ samba-cifs-autofs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 folder=Servidor
-#dominio=workgroup
+#domain=workgroup
 
 echo -e "\n=== Mapeando servidor Samba ===\n"
 read -p "Qual IP? " ip
@@ -10,6 +10,13 @@ read -s -p "Qual a senha? " pass
 echo ""
 read -p "Qual o nome da pasta? [padrão=Servidor] " folder
 #read -p "Qual o dominio / grupo de trabalho? [padrao=workgroup]" domain
+
+# Define diretório se existir
+if [ -e "/etc/autofs/" ]; then
+    autofsDir="/etc/autofs"
+elif [ -e "/etc/auto.master" ]; then
+    autofsDir="/etc"
+fi
 
 # Somente escreve se não tiver o texto do caminho, $1 é texto, $2 é caminho 
 escreveTextoSeNaoExistir(){
@@ -25,28 +32,28 @@ escreveTextoSeNaoExistir(){
 }
 
 # Escreve em auto.master caminho da montagem
-escreveTextoSeNaoExistir '/mnt/samba/ /etc/autofs/auto.samba --timeout 60 --browse' '/etc/autofs/auto.master'
+escreveTextoSeNaoExistir '/mnt/samba/ $autofsDir/auto.samba --timeout 60 --browse' '$autofsDir/auto.master'
 
 # Cria pasta de credenciais se não existir, e permite somente root
-if ! [ -e "/etc/autofs/credentials" ]; then
-    sudo mkdir /etc/autofs/credentials
-    sudo chown root:root /etc/autofs/credentials
-    sudo chmod 600 /etc/autofs/credentials
+if ! [ -e "$autofsDir/credentials/" ]; then
+    sudo mkdir $autofsDir/credentials/
+    sudo chown root:root $autofsDir/credentials/
+    sudo chmod 600 $autofsDir/credentials/
 fi
 
 # Monta samba
-escreveTextoSeNaoExistir "$ip-$folder -fstype=cifs,credentials=/etc/autofs/credentials/$ip-$folder.txt,noperm,nounix,file_mode=0777,dir_mode=0777 ://$ip/$folder" '/etc/autofs/auto.samba'
+escreveTextoSeNaoExistir "$ip-$folder -fstype=cifs,credentials=$autofsDir/credentials/$ip-$folder.txt,noperm,nounix,file_mode=0777,dir_mode=0777 ://$ip/$folder" '$autofsDir/auto.samba'
 
 # Salva as credenciais
 if [ "$domain" == "" ];then
-    sudo bash -c "echo -e \"user=$user\npassword=$pass\" > /etc/autofs/credentials/$ip-$folder.txt"
+    sudo bash -c "echo -e \"user=$user\npassword=$pass\" > $autofsDir/credentials/$ip-$folder.txt"
 else
-    sudo bash -c "sudo echo -e \"user=$user\npassword=$pass\ndomain=$domain\" > /etc/autofs/credentials/$ip-$folder.txt"
+    sudo bash -c "sudo echo -e \"user=$user\npassword=$pass\ndomain=$domain\" > $autofsDir/credentials/$ip-$folder.txt"
 fi
 
 # Tirar permissão para usuáios não root lerem arquivo de credenciais
-sudo chown root:root /etc/autofs/credentials/$ip-$folder.txt
-sudo chmod 600 /etc/autofs/credentials/$ip-$folder.txt
+sudo chown root:root $autofsDir/credentials/$ip-$folder.txt
+sudo chmod 600 $autofsDir/credentials/$ip-$folder.txt
 
 # Limpeza de variaveis
-unset ip user pass folder domain
+unset ip user pass folder domain autofsDir verificaTexto
